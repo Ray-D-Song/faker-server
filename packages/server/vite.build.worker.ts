a323ae9eebba0e95820c7f1037e81f24be9e5668
Ray-D-Song <songraysmail@gmail.com>
2024-10-12 17:21:58 +0800

    chore: config

--- packages/server/vite.build.worker.ts
+++ packages/server/vite.build.worker.ts
@@ -8,16 +8,6 @@ export default defineConfig({
       entry: './src/entry.worker.ts',
       outputDir: '../../dist',
     }),
-    generateWranglerToml(),
   ],
 })
 
-function generateWranglerToml() {
-  return {
-    name: 'generate-wrangler-toml',
-    closeBundle: () => {
-      const wranglerContent = fs.readFileSync('../../wrangler.toml').toString()
-      fs.writeFileSync('../../dist/wrangler.toml', wranglerContent)
-    },
-  }
-}
