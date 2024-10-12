d7119dd6ed5b67652a753d971d125709eb7bb7b1
Ray-D-Song <songraysmail@gmail.com>
2024-10-12 17:15:20 +0800

    chore: build config

--- packages/server/vite.build.worker.ts
+++ packages/server/vite.build.worker.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import build from '@hono/vite-build/cloudflare-pages'
+import fs from 'fs'
 
 export default defineConfig({
-  build: {
-    target: 'esnext',
-  },
   plugins: [
     build({
       entry: './src/entry.worker.ts',
       outputDir: '../../dist',
     }),
+    generateWranglerToml(),
   ],
 })
+
+function generateWranglerToml() {
+  return {
+    name: 'generate-wrangler-toml',
+    closeBundle: () => {
+      const wranglerContent = fs.readFileSync('../../wrangler.toml').toString()
+      fs.writeFileSync('../../dist/wrangler.toml', wranglerContent)
+    },
+  }
+}
